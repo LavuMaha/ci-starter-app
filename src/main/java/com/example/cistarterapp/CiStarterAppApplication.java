f63747271b6047dbf6ea6e9ff39e9f4a71d2eb82
LavuMaha <83201225+LavuMaha@users.noreply.github.com>
2021-10-05 15:17:24 -0700

    Add files via upload

---
+++ src/main/java/com/example/cistarterapp/CiStarterAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.cistarterapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiStarterAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CiStarterAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cistarterapp/CiStarterAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cistarterapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiStarterAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
